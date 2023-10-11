3ab28de3c0902f222b7ba16c204551c2d38c5555
Svetlana <A.svet.mr@yandex.ru>
2023-10-11 17:09:20 +0300

    v2

---
+++ src/Mass2Zadacha.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Mass2Zadacha {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(calculateMass(createMass())));
+    }
+
+    public static int createMass(){
+        System.out.println("Введите целое положительное число: ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        if (number>0)
+            return number;
+        else System.out.println("Введено не положительное число");
+        return number;
+    }
+    public static double[] calculateMass(int number) {
+        double[] mass = new double[number];
+
+        return mass;
+    }
+}
